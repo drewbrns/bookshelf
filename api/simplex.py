bf68c22e7f1c8f435ff46099d561183cc932c691
Drew Barnes <djblin@gmail.com>
2013-05-26 19:27:01 +0000

    Api version 0.1 done! :)

--- api/simplex.py
+++ api/simplex.py
@@ -32,18 +32,29 @@ def post_books():
 		
 	books = request.json
 	
-	if books:		
-		db['books'].insert(books);
-		return {"success": True }
-	else:
-		return {"success": False}
-
+	if books:
+		try:
+			db['books'].insert(books);
+			return {"response": {"success": True }}
+		except err:
+			return {"response": {"success": False, "error": err }}
 
 
 #PUT resource	
 @app.route('/books/:id', method = 'PUT')
 def put_book(id):
+	
+	book = request.json
+	
+	if book:
+		try: 
+			db['books'].update({'_id':id}, book)
+			return {"response": {"success": True}}
+		except err:
+			return {"response": {"success": False, "error": err } }
+	
 	return '''Editted an existing book.'''
+
 
 #DELETE resource
 @app.route('/books/:id', method = 'DELETE')
@@ -51,22 +62,9 @@ def delete_book(id):
 	try:
 		db['books'].remove({'_id':id})
 		return {"resonse": {"success": True}}
-	except delError:
-		return {"resonse": {"success": False}}
+	except err:
+		return {"resonse": {"success": False, "error": err }}
 
-
-@app.route('/documents', method='PUT')
-def put_document():
-    data = request.body.readline()
-    if not data:
-        abort(400, 'No data received')
-    entity = json.loads(data)
-    if not entity.has_key('_id'):
-        abort(400, 'No _id specified')
-    try:
-        db['documents'].save(entity)
-    except ValidationError as ve:
-        abort(400, str(ve))
   
 app.run()
 
